f6f287c1a86269dd99ee56ecaec952eea47a2779
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-31 15:12:24 -0300

    feat: added jwt token for registered user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,17 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { body } = req;
 
   const user = await User.create({ ...body });
 
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
