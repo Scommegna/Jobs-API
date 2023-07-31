fedb044a2c3d666233984315c49c5b14c2486daf
Lucas Scommegna <scommegnal@hotmail.com>
2023-07-31 15:51:31 -0300

    feat: JWT method for user schema

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -32,5 +33,12 @@ UserSchema.pre("save", async function (next) {
 
   next();
 });
+
+// Method that creates JWT key for new user
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id, name: this.name }, "jwtSecret", {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = mongoose.model("User", UserSchema);
